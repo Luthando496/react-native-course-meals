b8db1a5ab93e006480eeb20e575654fb3f068eb9
unknown <luthandodidiza197@gmail.com>
2023-09-04 11:44:27 +0200

    added favorites

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import { View, Text } from'react-native'
-
+import React,{useContext} from 'react'
+import { View, FlatList } from'react-native'
+import {FavoritesContext} from '../store/favoriteContext'
+import { MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem'
 
 
 const FavoritesScreen = () => {
+    const mealsContext = useContext(FavoritesContext)
+    const data = MEALS.filter(meal => mealsContext.ids.includes(meal.id))
   return (
-    <View>
-        <Text>
-            hello there luthando
-        </Text>
+    <View className='flex-1 p-3'>
+        <FlatList data={data} keyExtractor={(item)=> item.id} renderItem={(itemData)=>{
+        return (
+            <MealItem meal={itemData.item} duration={itemData.item.duration}
+            complexity={itemData.item.complexity} itemId={itemData.item.id} 
+            affordability={itemData.item.affordability} imageUrl={itemData.item.imageUrl} title={itemData.item.title}  />
+        )
+      }} />
     </View>
   )
 }
