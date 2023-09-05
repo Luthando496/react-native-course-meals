ad3682edf27e3b6412ce8248d48b4bbd62a914f3
unknown <luthandodidiza197@gmail.com>
2023-09-05 10:45:09 +0200

    added favorites if statement

--- screens/FavouritesScreen.js
+++ screens/FavouritesScreen.js
@@ -1,5 +1,5 @@
 import React,{useContext} from 'react'
-import { View, FlatList } from'react-native'
+import { View, FlatList,Text } from'react-native'
 import {FavoritesContext} from '../store/favoriteContext'
 import { MEALS } from '../data/dummy-data'
 import MealItem from '../components/MealItem'
@@ -8,6 +8,15 @@ import MealItem from '../components/MealItem'
 const FavoritesScreen = () => {
     const mealsContext = useContext(FavoritesContext)
     const data = MEALS.filter(meal => mealsContext.ids.includes(meal.id))
+
+    if(data.length === 0){
+        return (
+            <View className=' flex justify-center items-center flex-1'>
+                <Text className='text-2xl leading-[1.9]  text-center font-bold tracking-[3px] uppercase text-white'>No meals in your favorites</Text>
+            </View>
+        )
+    }
+
   return (
     <View className='flex-1 p-3'>
         <FlatList data={data} keyExtractor={(item)=> item.id} renderItem={(itemData)=>{
